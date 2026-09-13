f73abae231c7946e241ae782fe147e2ca029a353
SarathKannan <sarathdec2007@gmail.com>
2026-09-13 22:04:24 +0530

    Complete BST Contact Management System

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 struct Contact
@@ -47,6 +48,32 @@ Node* insertNode(Node *root , string name , string contact , string email,string
             root->right = insertNode(root->right, name , contact ,email,address);
         }
         return root;
+}
+
+void loadContact(Node *&root)
+{
+    ifstream infile("contacts.txt");
+
+    if(!infile.is_open())
+    {
+        return;
+    }
+    string line;
+    while(getline(infile,line))
+    {
+        string name, contact, email, address;
+
+        stringstream ss(line);
+
+        getline(ss, name, ',');
+        getline(ss, contact, ',');
+        getline(ss, email, ',');
+        getline(ss, address, ',');
+
+        root = insertNode(root, name, contact, email, address);
+    }
+
+    infile.close();
 }
 
 void InorderDisplay(Node *root)
@@ -184,11 +211,119 @@ void InorderDisplayFavourite(Node *root)
         }
         InorderDisplayFavourite(root->right);
 }
+
+void saveContactsToFile(Node *root,ofstream &outfile)
+{
+
+    if(root == nullptr)
+    {
+        return;
+    }
+    saveContactsToFile(root->left,outfile);
+    outfile << root->contact.name << ","<< root->contact.contact << ","<< root->contact.email << ","<< root->contact.address << endl;
+    saveContactsToFile(root->right,outfile);
+    
+}
+
+void saveContacts(Node *root)
+{
+    ofstream outfile("contacts.txt");
+
+    if(!outfile.is_open())
+    {
+        return;
+    }
+
+    saveContactsToFile(root, outfile);
+
+    outfile.close();
+}
+
+void editContact(Node *root, string name)
+{
+    Node *result = searchContactbyName(root, name);
+
+    if(result == nullptr)
+    {
+        cout << "Contact not found.\n";
+        return;
+    }
+    cout << "\nWhat do you want to edit?\n";
+    cout << "1. Contact Number\n";
+    cout << "2. Email\n";
+    cout << "3. Address\n";
+    cout << "Enter your choice: ";
+    int choice;
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1:
+            {
+                string contact;
+                cout << "Enter the new contact Number : ";
+                cin >> contact;
+                if(searchContactbyContact(root,contact) != nullptr)
+                {
+                    cout << "Contact number already exists. Please enter a unique number.\n";
+                    break;
+                }
+                result->contact.contact = contact;
+                break;
+            }
+
+        case 2:
+            {
+                string email;
+
+                while(true)
+                {
+                    cout << "Enter the new email: ";
+                    cin >> email;
+
+                    try
+                    {
+                        if(email.find("@gmail.com") == string::npos)
+                         {
+                            throw "Invalid email address!";
+                        }
+
+                         break;
+                    }
+                    catch(const char* msg)
+                    {
+                        cout << msg << " Please try again.\n";
+                     }
+                }
+
+                result->contact.email = email;
+
+                cout << "Email updated successfully!\n";
+                break;
+            }
+        
+        case 3:
+            {
+                string address;
+
+                cout << "Enter the new address: ";
+                cin.ignore();
+                getline(cin, address);
+
+                result->contact.address = address;
+
+                cout << "Address updated successfully!\n";
+                break;
+            }
+    }
+    
+}
+
 int main()
 {
     Node *root = nullptr;
     int choice;
-
+    loadContact(root);
     do
     {
         cout << "\n========== CONTACT MANAGEMENT SYSTEM ==========\n";
@@ -198,7 +333,8 @@ int main()
         cout << "4. Display Contacts\n";
         cout << "5. Mark / Unmark Favourite\n";
         cout << "6. Display Favourite Contacts\n";
-        cout << "7. Exit\n";
+        cout << "7. Edit Contact\n";
+        cout << "8. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -209,13 +345,35 @@ int main()
                 string name, contact, email,address;
 
                 cout << "Enter name: ";
-                cin >> name;
+                cin.ignore();
+                getline(cin, name);
 
                 cout << "Enter contact number: ";
                 cin >> contact;
 
-                cout << "Enter email: ";
-                cin >> email;
+                if(searchContactbyContact(root,contact) != nullptr)
+                {
+                    cout << "Contact number already exists. Please enter a unique contact number.\n";
+                    break;
+                }               
+                
+                while(true)
+                {
+                    cout << "Enter email: ";
+                    cin >> email;
+                    try
+                    {
+                        if(email.find("@gmail.com") == string::npos)
+                        {
+                            throw "Invalid email Address!. " ;
+                        }
+                        break;
+                    }
+                    catch(const char* msg)
+                    {
+                        cout << msg << " Please try again.\n";
+                    }
+                }
 
                 cout << "Enter address: ";
                 cin.ignore();
@@ -224,13 +382,6 @@ int main()
                 root = insertNode(root, name, contact, email,address);
 
                 cout << "Contact added successfully!\n";
-
-                ofstream outfile("contacts.txt", ios::app);
-                if(outfile.is_open())
-                {
-                    outfile << name << "," << contact << "," << email << "," << address << endl;
-                    outfile.close();
-                }
                 break;
             }
 
@@ -359,6 +510,16 @@ int main()
                 break;
             }
             case 7:
+            {
+                string name;
+                cout << "Enter the name of contact ";
+                cin >> name;
+                editContact(root,name);
+                break;
+            }
+
+            case 8:
+                saveContacts(root);
                 cout << "Exiting Contact Management System...\n";
                 break;
 
@@ -366,7 +527,7 @@ int main()
                 cout << "Invalid choice! Please try again.\n";
         }
 
-    } while(choice != 7 );
+    } while(choice != 8 );
 
     return 0;
 }
